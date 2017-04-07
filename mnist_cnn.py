870bea48508c821d2ac193ebc9ceab203cae9d94
Fredrik <fredrikcch@msn.com>
2017-04-07 16:07:39 +0200

    Downloading images and labels

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -1,4 +1,23 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as pyplt
 
-"""Hello there"""
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+train_images = mnist.train.images
+train_labels = mnist.train.labels
+
+train_set = np.array(train_images, dtype=np.float32)
+num_samples = len(train_set)
+train_set = train_set.reshape([num_samples, 28, 28])
+
+# print(train_labels[6])
+# pyplt.imshow(train_set[6], cmap='gray')
+# pyplt.show()
+
