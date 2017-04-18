32a99d2d8de4ed06afe30ed6a10cbda62b07aea5
Fredrik <fredrikcch@msn.com>
2017-04-18 10:49:02 +0200

    Completed the ffwd script and added a model saver

--- mnist_ffwd.py
+++ mnist_ffwd.py
@@ -141,9 +141,9 @@ if __name__ == '__main__':
                         default=False, help='If true, uses fake data for unit testing')
     parser.add_argument('--max_steps', type=int, default=5000, help='Number of steps to run trainer')
     parser.add_argument('--learning_rate', type=float, default=0.01, help='Initial learning rate')
-    parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+    parser.add_argument('--data_dir', type=str, default='C:/Users/fredr/PycharmProjects/TensorFlowTutorial/MNIST_data',
                         help='Directory for storing input data')
-    parser.add_argument('--log_dir', type=str, default='/tmp/tensorflow/mnist/logs/mnist_with_summaries',
+    parser.add_argument('--log_dir', type=str, default='C:/Users/fredr/PycharmProjects/TensorFlowTutorial/log/mnist_with_summaries',
                         help='Summaries log directory')
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
